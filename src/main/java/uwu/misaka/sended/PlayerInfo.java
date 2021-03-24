f87f2830b7bbb1b573297ac6dda7679b16f459ef
Misaka <misakachan2545@gmail.com>
2021-03-24 20:19:21 +0300

    РЕ дизайн

--- src/main/java/uwu/misaka/sended/PlayerInfo.java
+++ src/main/java/uwu/misaka/sended/PlayerInfo.java
@@ -6,19 +6,19 @@ import static uwu.misaka.Ichi.gson;
 
 public class PlayerInfo {
     public String uuid; //primary key
-    public int blocksPlaced=0;
-    public int blocksDestroyed=0;
-    public long totalPlayedTime =0;
-    public int pvpWins=0;
-    public int hexedWins=0;
-    public int attackWins=0;
-    public int totalWaves=0;
-    public long discordId=0;
+    public int blocksPlaced = 0;
+    public int blocksDestroyed = 0;
+    public long totalPlayedTime = 0;
+    public int pvpWins = 0;
+    public int hexedWins = 0;
+    public int attackWins = 0;
+    public int totalWaves = 0;
+    public long discordId = 0;
     public String nick = "";
     public ArrayList<String> warnings = new ArrayList<>();
 
-    public PlayerInfo(String uuid){
-        this.uuid=uuid;
+    public PlayerInfo(String uuid) {
+        this.uuid = uuid;
     }
     public static PlayerInfo fromJson(String json){
         return gson.fromJson(json, PlayerInfo.class);
